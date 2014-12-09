e74dd03f1bcaadf2b196a1ce69c7c959dc8925fd
Rafael Silva <rafael.souza.prog@gmail.com>
2014-12-09 13:26:28 -0200

    #fix the settings

--- src/settings.py
+++ src/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'minitickets',
+        'USER': 'minitickets',
+        'PASSWORD': 'minitickets',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
